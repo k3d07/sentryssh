a27872453e57bd6682f0da13d1a0d418c974a9ba
k3d07 <kedrich.custodio@gmail.com>
2026-09-14 13:26:57 +0800

    optimized: (parse.sh): added quotation mark in the log_file l.4 | fixed the small bug in regex l.7 | removed  the else/continue since its irrelevant

--- lib/parse.sh
+++ lib/parse.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 parse_log() {
-    local log_file=$1
+    local log_file="$1"
     while IFS= read -r line; do
         if [[ "$line" == *"Failed password"* ]]; then
-            if [[ "$line" =~ ([A-za-z]{3}[[:space:]]+[0-9]{1,2}[[:space:]]+[0-9:]{8}).*Failed\ password\ for\ (invalid\ user\ )?([^[:space:]]+)\ from\ ([0-9.]+) ]]; then
-                printf "IP: ${BASH_REMATCH[4]} | User: ${BASH_REMATCH[3]} | Time: ${BASH_REMATCH[1]}\n"
+            if [[ "$line" =~ ([A-Za-z]{3}[[:space:]]+[0-9]{1,2}[[:space:]]+[0-9:]{8}).*Failed\ password\ for\ (invalid\ user\ )?([^[:space:]]+)\ from\ ([0-9.]+) ]]; then
+                printf "IP: %s | User: %s | Time: %s\n" "${BASH_REMATCH[4]}" "${BASH_REMATCH[3]}" "${BASH_REMATCH[1]}"
             fi
-        else
-            continue
         fi
     done < "$log_file"
 }
